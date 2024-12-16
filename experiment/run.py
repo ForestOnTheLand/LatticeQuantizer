475df1a5ece4aad8748e40a272430b77b0e3fb7b
Deng Jingwen <2200013100@stu.pku.edu.cn>
2024-12-16 05:32:12 +0000

    final results and plots

--- experiment/run.py
+++ experiment/run.py
@@ -5,36 +5,51 @@ from pathlib import Path
 import json
 import argparse
 
-from lattice.construct import construction
+from lattice import classical
+from lattice.construct import construction, orthogonalize
 from lattice.nsm import numerical_nsm
 from lattice.visualize import close_points_plot
+from datetime import datetime
+
+def visualize(dimension: int, A, plot_dir: Path):
+    plt.figure()
+    try:
+        A_star = orthogonalize(classical.best_lattice(dimension))
+        x, y = close_points_plot(A_star)
+        plt.plot(x, y, label="optimal")
+        plt.yscale("log")
+    except NotImplementedError:
+        print(f"Unknown Best Lattice in dimension {dimension}.")
+
+    x, y = close_points_plot(A)
+    plt.plot(x, y, label="numerical")
+    plt.yscale("log")
+    plt.legend()
+    plt.savefig(plot_dir / f"{dimension}.png")
 
 @ray.remote
 def task(dimension: int, output_path: Path, plot_dir: Path, **config):
     A = construction(dimension, **config, progress_bar=False)
-    error, var = numerical_nsm(A, num_process=16)
+    error, var = numerical_nsm(A, num_process=8)
     print(f"Dimension {dimension}. Estimated NSM: {error} with variance {var}")
     with open(output_path, "a") as file:
         file.write(json.dumps({"dimension": dimension, "matrix": A.tolist() ,"error": error, "variance": var}) + "\n")
-    x, y = close_points_plot(A)
-    plt.plot(x, y)
-    plt.yscale("log")
-    plt.savefig(plot_dir / f"{dimension}.png")
+    visualize(dimension, A, plot_dir)
 
 if __name__ == "__main__":
-    # Example usage: python run.py fast --min_dimension 10 --max_dimension 16
+    # Example usage: python run.py [config_name] --dimensions 10 11 12 13 14 15 16
     ray.init()
     parser = argparse.ArgumentParser()
     parser.add_argument("config_name", type=str)
-    parser.add_argument("--min_dimension", type=int, default=10)
-    parser.add_argument("--max_dimension", type=int, default=16)
+    parser.add_argument("--dimensions", type=int, nargs='+', help="List of dimensions to run", default=[10, 11, 12, 13, 14, 15, 16])
     args = parser.parse_args()
     config_name = args.config_name
     config = json.load(open(f"configs/{config_name}.json"))
-    output_path = Path(f"results/{config_name}.jsonl")
-    plot_dir = Path(f"plots/{config_name}")
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+    output_path = Path(f"results/{config_name}_{timestamp}.jsonl")
+    plot_dir = Path(f"plots/{config_name}_{timestamp}")
     plot_dir.mkdir(parents=True, exist_ok=True)
-    candidate_dimensions = range(args.min_dimension, args.max_dimension + 1)
+    candidate_dimensions = args.dimensions
     # skip all dimensions that have been computed
     if output_path.exists():
         with open(output_path, "r") as file:
