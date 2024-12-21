fdf762bebd0ada94c5ea6d7bec51470dd86ddb40
Deng Jingwen <2200013100@stu.pku.edu.cn>
2024-12-21 08:37:18 +0000

    [Update] A faster method to find best lattice.

--- experiment/run.py
+++ experiment/run.py
@@ -11,6 +11,8 @@ from lattice.nsm import numerical_nsm
 from lattice.visualize import close_points_plot
 from datetime import datetime
 
+NUM_TRIALS = 20
+
 def visualize(dimension: int, A, plot_dir: Path):
     plt.figure()
     try:
@@ -28,8 +30,24 @@ def visualize(dimension: int, A, plot_dir: Path):
     plt.savefig(plot_dir / f"{dimension}.png")
 
 @ray.remote
-def task(dimension: int, output_path: Path, plot_dir: Path, **config):
+def task(dimension: int, config_name: str):
+    config = json.load(open(f"configs/{config_name}.json"))
     A = construction(dimension, **config, progress_bar=False)
+    error, var = numerical_nsm(A, num_process=4)
+    return A, error, var
+
+@ray.remote
+def search(dimension: int, output_path: Path, plot_dir: Path):
+    '''
+    Search for the best lattice in the given dimension.
+    First run 'medium' config for 20 times to find a good starting point.
+    Then use the matrix with lowest NSM as checking point to run 'slow' config.
+    '''
+    tasks = [task.remote(dimension, "medium") for _ in range(NUM_TRIALS)]
+    results = ray.get(tasks)
+    best_A = min(results, key=lambda x: x[1])[0]
+    print(f"Dimension {dimension}. Best matrix from medium config found.")
+    A = construction(dimension, **json.load(open("configs/slow.json")), progress_bar=False, checkpoint=best_A)
     error, var = numerical_nsm(A, num_process=8)
     print(f"Dimension {dimension}. Estimated NSM: {error} with variance {var}")
     with open(output_path, "a") as file:
@@ -37,17 +55,14 @@ def task(dimension: int, output_path: Path, plot_dir: Path, **config):
     visualize(dimension, A, plot_dir)
 
 if __name__ == "__main__":
-    # Example usage: python run.py [config_name] --dimensions 10 11 12 13 14 15 16
+    # Example usage: python run.py --dimensions 10 11 12 13 14 15 16
     ray.init()
     parser = argparse.ArgumentParser()
-    parser.add_argument("config_name", type=str)
     parser.add_argument("--dimensions", type=int, nargs='+', help="List of dimensions to run", default=[10, 11, 12, 13, 14, 15, 16])
     args = parser.parse_args()
-    config_name = args.config_name
-    config = json.load(open(f"configs/{config_name}.json"))
-    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-    output_path = Path(f"results/{config_name}_{timestamp}.jsonl")
-    plot_dir = Path(f"plots/{config_name}_{timestamp}")
+    timestamp = datetime.now().strftime("%Y%m%d")
+    output_path = Path(f"results/{timestamp}.jsonl")
+    plot_dir = Path(f"plots/{timestamp}")
     plot_dir.mkdir(parents=True, exist_ok=True)
     candidate_dimensions = args.dimensions
     # skip all dimensions that have been computed
@@ -58,5 +73,5 @@ if __name__ == "__main__":
     else:
         dimensions = candidate_dimensions
     print(f"{len(candidate_dimensions)-len(dimensions)} dimensions have been computed, {len(dimensions)} dimensions to go.")
-    tasks = [task.remote(dimension, output_path, plot_dir, **config) for dimension in dimensions]
+    tasks = [search.remote(dimension, output_path, plot_dir) for dimension in dimensions]
     ray.get(tasks)
